7bb5a1f73010b92c0bddd9f569b8f9522fd157e4
Daniel Gempesaw <gempesaw@gmail.com>
2015-07-07 15:33:22 -0400

    Implement getting a list of builds from Jenkins

--- lib/Honeydew/Jenkins/BuildLookup.pm
+++ lib/Honeydew/Jenkins/BuildLookup.pm
@@ -3,6 +3,14 @@ package Honeydew::Jenkins::BuildLookup;
 use strict;
 use warnings;
 
+use feature qw/state/;
+use DBD::MySQL;
+use Honeydew::Config;
+use HTTP::Tiny;
+use JSON;
+use MIME::Base64;
+use Moo;
+
 # ABSTRACT:
 
 =for markdown [![Build Status](https://travis-ci.org/honeydew-sc/Honeydew-Jenkins-BuildLookup.svg?branch=master)](https://travis-ci.org/honeydew-sc/Honeydew-Jenkins-BuildLookup)
@@ -12,5 +20,81 @@ use warnings;
 =head1 DESCRIPTION
 
 =cut
+
+has config => (
+    is => 'lazy',
+    default => sub {
+        return Honeydew::Config->instance;
+    }
+);
+
+has jenkins_base_url => (
+    is => 'lazy',
+    default => sub {
+        my ($self) = @_;
+
+        return $self->config->{jenkins}->{base_url};
+    }
+);
+
+has jenkins_auth => (
+    is => 'lazy',
+    default => sub {
+        my ($self) = @_;
+
+        my $auth = $self->config->{jenkins}->{auth};
+        my $b64_auth = encode_base64($auth);
+        chomp $b64_auth;
+        return $b64_auth;
+    }
+);
+
+has ua => (
+    is => 'lazy',
+    default => sub {
+        return HTTP::Tiny->new;
+    }
+);
+
+sub get_builds {
+    my ($self, %args) = @_;
+
+    my $url = $self->_get_runner_url( %args );
+    my $builds = $self->__get_json( url => $url );
+}
+
+sub __get_json {
+    my ($self, %args) = @_;
+    my $ua = $self->ua;
+
+    my $options = $self->__get_jenkins_headers;
+
+    my $res = $ua->get( $args{url}, $options );
+    return from_json( $res->{content} );
+}
+
+sub __get_jenkins_headers {
+    my ($self) = @_;
+
+    state $options = {
+        headers => {
+            Authorization => 'Basic ' . $self->jenkins_auth
+        }
+    };
+
+    return $options;
+}
+
+sub _get_runner_url {
+    my ($self, %args) = @_;
+    my $runner = $args{runner};
+
+    my $base = $self->jenkins_base_url;
+
+    return $base .
+      '/job/' .
+      $runner .
+      '/api/json?tree=builds[url,number]';
+}
 
 1;
--- t/Honeydew-Jenkins-BuildLookup.t
+++ t/Honeydew-Jenkins-BuildLookup.t
@@ -1,5 +1,44 @@
 use strict;
 use warnings;
 use Test::Spec;
+use Honeydew::Jenkins::BuildLookup;
 
-runtests
+describe 'Jenkins Build Lookup' => sub {
+    my ($jenkins, $mock_ua);
+    before each => sub {
+        $mock_ua = mock();
+        my $config = mock_config();
+        $jenkins = Honeydew::Jenkins::BuildLookup->new(
+            config => $config,
+            ua => $mock_ua
+        );
+    };
+
+    it 'should construct build runner urls' => sub {
+        my $runners = $jenkins->_get_runner_url( runner => 'test-runner' );
+        like( $runners, qr{job/test-runner/api/json\?tree=builds\[url,number\]} );
+    };
+
+    it 'should get builds for a runner' => sub {
+        $mock_ua->expects('get')
+          ->with_deep( 'jenkins_base_url/job/test-runner/api/json?tree=builds[url,number]', {
+              headers => {
+                  Authorization => 'Basic amVua2luc19hdXRo'
+              }
+          })
+          ->returns({ content => '{"json":"json"}' });
+        my $build_data = $jenkins->get_builds( runner => 'test-runner' );
+        is_deeply( $build_data, { json => 'json' } );
+    };
+};
+
+sub mock_config {
+    return {
+        jenkins => {
+            base_url => 'jenkins_base_url',
+            auth => 'jenkins_auth'
+        }
+    };
+}
+
+runtests;
