88c301c84ab6981fe0e0b224db9a63df32283eaa
Daniel Gempesaw <gempesaw@gmail.com>
2015-07-07 12:07:02 -0400

    initial commit

---
+++ lib/Honeydew/Jenkins/BuildLookup.pm
@@ -0,0 +1,16 @@
+package Honeydew::Jenkins::BuildLookup;
+
+use strict;
+use warnings;
+
+# ABSTRACT:
+
+=for markdown [![Build Status](https://travis-ci.org/honeydew-sc/Honeydew-Jenkins-BuildLookup.svg?branch=master)](https://travis-ci.org/honeydew-sc/Honeydew-Jenkins-BuildLookup)
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+1;
